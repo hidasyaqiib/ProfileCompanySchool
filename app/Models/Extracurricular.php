8f70d561b81f11cb6a6405f690669d6d55b8eb05
MJordanWS <soebrotojordan@gmail.com>
2026-03-08 22:01:28 +0700

    Backend Kurikulum dkk

---
+++ app/Models/Extracurricular.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Extracurricular extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+}
